37cc571dd998452784fecf2beff3a70e3c3491d3
UsernameAlexei <63068458+UsernameAlexei@users.noreply.github.com>
2021-06-02 12:42:41 +0300

    Update Testing_Coverage_task.py

--- Testing_Coverage_task.py
+++ Testing_Coverage_task.py
@@ -1,39 +1,36 @@
-#Task1
+# Task1
 def my_range(start=0, stop=None, step=1):
-
     if stop is None:
         stop = start
         start = 0
 
-    if start < stop and step>0:
-        while start<stop:
+    if start < stop and step > 0:
+        while start < stop:
             yield start
-            start+=step
+            start += step
     else:
-        while start>stop and step<0:
+        while start > stop and step < 0:
             yield start
-            start+=step
+            start += step
 
-#Task2
+
+# Task2
+global CACHE
+CACHE = {}
+
+
 def cached(use_cache):
     def use(func):
-        cache = {}
 
         def a(*args, **kwargs):
-            nonlocal cache
 
-            if not cache.get(args):
+            if not CACHE.get(args) or not use_cache:
                 g = func(*args, **kwargs)
-                cache[args] = g
-                return f'caching {g}'
-
-            elif not use_cache:
-                g = func(*args, **kwargs)
-                cache[args] = g
-                return f'caching {g}'
+                CACHE[args] = g
+                return g
 
             else:
-                return f'from cache {cache[args]}'
+                return CACHE[args]
 
         return a
 
