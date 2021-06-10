2aebe22d7ec6e3675f600d8acc33d7f8d94af034
UsernameAlexei <63068458+UsernameAlexei@users.noreply.github.com>
2021-06-10 21:32:47 +0300

    Update Profiling_task.py

--- Profiling_task.py
+++ Profiling_task.py
@@ -1,67 +1,57 @@
 # TASK 1-4
-# 1 read and write way 1
+
 import csv
 import pandas as pd
 
 
-def football_1(x=None):
+# 1 read and write way 1
+def read_write_1(x=None, data=None):
     if x is None:
         return None
     else:
-        df = pd.read_csv('football_results.csv')
+        df = pd.read_csv(data)
         df_dict = df.to_dict()
         return df_dict[x]
 
 
 # 1 read and write way 2
-def football_2(x=None):
+def read_write_2(x=None, data=None):
     d = {}
     if x is None:
         return None
     else:
-        reader = csv.DictReader(open('football_results.csv'))
+        reader = csv.DictReader(open(data))
         for i, s in enumerate(reader):
             d[i] = s[x]
     return d
 
 
 # 2 list transformation way 1
-def list_transformation_1(City):
-    City_1 = []
-    for i in City:
-        City_1.append(i.upper())
-    return City_1
+def list_transformation_1(cities):
+    cities_1 = []
+    for i in cities:
+        cities_1.append(i.upper())
+    return cities_1
 
 
 # 2 list transformation way 2
-def list_transformation_2(City):
-    City_2 = [x.upper() for x in City]
-    return City_2
+def list_transformation_2(cities):
+    cities_2 = [x.upper() for x in cities]
+    return cities_2
 
 
 # 2 list transformation way 3
-def list_transformation_3(City):
-    City_3 = list(map(lambda x: x.upper(), City))
-    return City_3
+def list_transformation_3(cities):
+    cities_3 = list(map(lambda x: x.upper(), cities))
+    return cities_3
 
 
 # 3 sorting the list way 1
-def bubble_sort(arr):
-    def swap(i, j):
-        arr[i], arr[j] = arr[j], arr[i]
-
-    n = len(arr)
-    swapped = True
-
-    x = -1
-    while swapped:
-        swapped = False
-        x = x + 1
-        for i in range(1, n - x):
-            if arr[i - 1] > arr[i]:
-                swap(i - 1, i)
-                swapped = True
-    return arr
+def pd_sort_values(arr):
+    df = {}
+    df['number'] = arr
+    df = pd.DataFrame(df)
+    return df.sort_values(by='number')
 
 
 # 3 sorting the list way 2
@@ -78,15 +68,36 @@ def selection_sort(arr):
     return arr
 
 
+# 3 sorting the list way 2
+def method_sorted(arr):
+    return sorted(arr)
+
+
 # 4 unpacking a nested list way 1
-def unpacking_1(un_list):
-    up_1 = " ".join(" ".join(L) for L in un_list)
-    return up_1
+# calculates the depth of the inner list
+def count_list(l):
+    count = 0
+    for e in l:
+        if isinstance(e, list):
+            count = count + 1 + count_list(e)
+    return count
+
+
+def unpacking_1(unpacking_list):
+    s = []
+    for i in range(count_list(unpacking_list)):
+        if i >= 1:
+            unpacking_list = s
+            s = []
+        for y in range(len(unpacking_list)):
+            try:
+                for x in unpacking_list[y]:
+                    s.append(x)
+            except TypeError:
+                s.append(unpacking_list[y])
+    return unpacking_list
 
 
 # 4 unpacking a nested list way 2
-def unpacking_2(un_list):
-    up_2 = []
-    for i in un_list:
-        up_2 += i
-    return " ".join(up_2)
+def unpacking_2(unpacking_list):
+    return [next_value for next_list in unpacking_list for next_value in next_list]
