eb96243599bfb55555b570221524b01e30d3809c
UsernameAlexei <63068458+UsernameAlexei@users.noreply.github.com>
2021-07-23 00:27:30 +0300

    Update urls.py

--- Django_web/myfirst/urls.py
+++ Django_web/myfirst/urls.py
@@ -17,8 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('articles/', include('articles.urls')),
+    path('', include('articles.urls')),
     path('grappelli/', include('grappelli.urls')),
     path('admin/', admin.site.urls),
 ]
-
