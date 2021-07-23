c4e6d7b57c176f220118db8d5568ba6a1149c4c6
UsernameAlexei <63068458+UsernameAlexei@users.noreply.github.com>
2021-07-23 17:26:18 +0300

    Update urls.py

--- Django_web/myfirst/apps/articles/urls.py
+++ Django_web/myfirst/apps/articles/urls.py
@@ -4,10 +4,10 @@ from . import views
 
 app_name = "articles"
 
-
-urlpatterns =[
-    path('', views.home, name = 'base'),
-    path('articles/', views.index, name = 'index'),
-    path('<int:article_id>/', views.detail, name = 'detail'),
-    path('<int:article_id>/leave_comment/', views.leave_comment, name = 'leave_comment')
+urlpatterns = [
+    path('', views.home, name='base'),
+    path('articles/', views.index, name='index'),
+    path('<int:article_id>/', views.detail, name='detail'),
+    path('<int:article_id>/leave_comment/', views.leave_comment,
+         name='leave_comment')
 ]
